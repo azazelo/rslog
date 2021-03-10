d723b51395ea2a0009ea47e60f0fee931fe8b4de
Andrey Eremeev <andrey.eremeyev@gmail.com>
2021-03-10 16:37:35 +0400

    clean code

--- lib/rslog/extractor.rb
+++ lib/rslog/extractor.rb
@@ -4,18 +4,21 @@
 #
 class Extractor < Worker
   def execute
-    @file_name = container.file_name
     check
     self
   end
 
   private
 
+  def file_name
+    container.file_name
+  end
+  
   def check
-    if File.zero?(@file_name)
+    if File.zero?(file_name)
       container.add_message 'Empty file'
     else
-      container.data = File.open(@file_name, 'r').to_a
+      container.data = File.open(file_name, 'r').to_a
       container.add_message 'Data in place'
     end
   rescue StandardError => e
